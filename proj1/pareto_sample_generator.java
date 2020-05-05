30df17cf665219a5523d6eaafb3cefbaae0af358
Jui-yang Cheng <46548160+KURO-AKA-BLACK@users.noreply.github.com>
2020-05-05 00:15:41 -0700

    Update pareto_sample_generator.java

--- proj1/pareto_sample_generator.java
+++ proj1/pareto_sample_generator.java
@@ -4,8 +4,8 @@ import java.lang.Math;
 
 public class RandomGen {
 	public static double pareto_generator (double rate) {
-		double scale = 6820;
-		double shape = 4;
+		double scale = 1;
+		double shape = 1;
 		return (scale/(Math.pow((1-rate), (1/shape))));
 	}
 }
